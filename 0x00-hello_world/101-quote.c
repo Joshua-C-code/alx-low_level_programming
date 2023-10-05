4cb9a495fcbe0392413fad08e98e2de44e0f6b84
Eze Joshua <joshuaeze87@gmail.com>
2023-10-05 14:47:06 -0700

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <unixstd.h>
-
+#include <unistd.h>
 /**
  * main - This is a c program that prints a line to the standard error
  * Return: 1 (Successful)
  */
 int main(void)
-
 {
-	fput ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);	
 }	
