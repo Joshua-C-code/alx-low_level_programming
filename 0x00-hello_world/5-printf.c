e2e0da5f6ccdaf6c4f69194a2fa2316e045ad3df
Eze Joshua <joshuaeze87@gmail.com>
2023-10-05 12:50:09 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this function is the main
+ *
+ * Return: 0 when correct
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
