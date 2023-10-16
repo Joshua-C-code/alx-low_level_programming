ee25a169d3fc6fc27fb3b28636ad025c01ee17dd
Eze Joshua <joshuaeze87@gmail.com>
2023-10-16 13:35:42 -0700

    last

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,25 +2,58 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 13
+/**
+ * main - generates random valid passwords
+ * Return: void
+ */
 
-char generateRandomChar() {
-	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	int index = rand() % (sizeof(charset) - 1);
-	return charset[index];
-}
+int main(void)
+{
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-int main(void) {
-	srand(time(NULL));
+	srand(time(0));
+	
+	while (sum < 2772)
 
-	char password[PASSWORD_LENGTH + 1];
-
-	for (int i = 0; i < PASSWORD_LENGTH; i++) {
-		password[i] = generateRandomChar();
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
-	password[PASSWORD_LENGTH] = '\0';
 
-	printf("%s\n", password);
+	password[index] = '\0';
 
-	return 0;
+	if (sum!= 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+
+		if ((sum - 2772) % 2 != 0)
+		diff_half1++;
+
+		for (index = 0; password[index]; index++)
+
+		{
+			if (password[index] >= (33 + diff_half1))
+			
+			{
+				password[index] -= diff_half1;
+
+				break;
+			}
+		}
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+
+			{
+				password[index] -= diff_half2;
+				
+				break;
+			}
+		}
+	}
+	printf("%s", password);
+	return (0);
 }
